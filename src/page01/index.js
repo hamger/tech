cfeae980fcc9490f9b0dc60f6e9c34f44cf2bf71
hamger <2313303800@qq.com>
2019-02-28 11:41:26 +0800

    add dom render

--- src/page01/index.js
+++ src/page01/index.js
@@ -1,13 +1,13 @@
 import './index.scss'
 document.write('<h1>page01</h1>')
 
-function Promise (fn) {
+function Promis (fn) {
   var state = 'pending',
     value = null,
     callbacks = []
 
   this.then = function (onFulfilled) {
-    return new Promise(function (resolve) {
+    return new Promis(function (resolve) {
       handle({
         onFulfilled: onFulfilled || null,
         resolve: resolve
@@ -20,12 +20,12 @@ function Promise (fn) {
     if (state === 'pending') {
       callbacks.push(callback)
       return
-    } // 如果then中没有传递任何东西
+    }
+    // 如果then中没有传递任何东西，就参数渗透
     if (!callback.onFulfilled) {
       callback.resolve(value)
       return
     }
-
     var ret = callback.onFulfilled(value)
     callback.resolve(ret)
   }
@@ -35,7 +35,7 @@ function Promise (fn) {
       newValue &&
       (typeof newValue === 'object' || typeof newValue === 'function')
     ) {
-      // 转入的是 promise 实例的情况统一都放入 callbacks
+      // 转入的是 promise 实例的情况
       var then = newValue.then
       if (typeof then === 'function') {
         then.call(newValue, resolve)
@@ -53,3 +53,46 @@ function Promise (fn) {
 
   fn(resolve)
 }
+
+var p = new Promis(function (resolve) {
+  // 遇到异步事件，挂起到 bar 到宏任务队列
+  setTimeout(function bar () {
+    resolve(1)
+  })
+})
+// 通过 then 方法注册回调函数 foo 和 foo2
+/**
+ * 注册后，p 的 callbacks = [
+    {
+      onFulfilled: foo,
+      resolve: resolve
+    }
+  ]，
+  p2 的 callbacks = [
+    {
+      onFulfilled: foo2,
+      resolve: resolve
+    }
+  ]
+ */
+var p2
+p.then(function foo (value) {
+  console.log(value)
+  p2 = new Promis(resolve => {
+    setTimeout(function bar2 () {
+      resolve(value * 2)
+    })
+  })
+  return p2
+}).then(function foo2 (value) {
+  console.log(value)
+})
+console.log(p2)
+// 同步代码执行完毕，接下来执行 bar 函数
+/**
+ * 1. resolve(1)
+ * 2. state = 'fulfilled'; value = 1
+ * 3. 遍历 callbacks，依次调用 handle(callback)
+ * 4. resolve(foo(value))，foo(value) 返回的是 Promis 实例，因此执行 p2.then(resolve),
+ * 得到 p2 的 callbacks
+ */
--- src/page02/index.js
+++ src/page02/index.js
@@ -1,33 +1,33 @@
 import './index.scss'
 document.write('<h1>page02</h1>')
 
-function curry(fn) {
-	var arg = []
-	return function () {
+function curry (fn) {
+  var arg = []
+  return function () {
     console.log(arg)
-		if (arg.length === 3) return fn.apply(this, arg)
-		arg.concat([].slice.call(arguments))
-		return this
-		// return arguments.callee
-	}
+    if (arg.length === 3) return fn.apply(this, arg)
+    arg.concat([].slice.call(arguments))
+    return this
+    // return arguments.callee
+  }
 }
 
-function add(a, b, c) {
-	return a + b + c
+function add (a, b, c) {
+  return a + b + c
 }
 
 var newAdd = curry(add)
 var res = newAdd(1, 3)(2)
 console.log(res)
 
-function foo() {
-	console.log('foo')
-	Promise.resolve().then(foo)
+function foo () {
+  console.log('foo')
+  Promise.resolve().then(foo)
 }
 foo()
-function bar() {
-	console.log('bar')
-	setTimeout(bar)
+function bar () {
+  console.log('bar')
+  setTimeout(bar)
 }
 bar()
 // foo bar 然后一直输出 'foo'
