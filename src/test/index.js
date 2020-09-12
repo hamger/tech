ae3c8e67cc22611158510c358fa5c1f1e847aeb1
Hanger Meng <hangermeng@gmail.com>
2020-09-12 13:06:29 +0800

    update

--- src/test/index.js
+++ src/test/index.js
@@ -45,61 +45,61 @@
 //  * @return {number[][]}
 //  */
 var combine = function (n, k) {
-    var res = []
-    var selects = new Array(n).fill(1).map((item, index) => index)
-    function dfs(path, selects) {
-        if (path.length === k) {
-            res.push(JSON.parse(JSON.stringify(path)))
-            return
-        }
-        selects.forEach((item, index) => {
-            path.push(item)
-            dfs(path, selects.filter((item, index2) => {
-                return index2 !== index
-            }))
-            path.pop()
-        })
+  var res = []
+  var selects = new Array(n).fill(1).map((item, index) => index)
+  function dfs (path, selects) {
+    if (path.length === k) {
+      res.push(JSON.parse(JSON.stringify(path)))
+      return
     }
-    dfs([], selects)
-    return res
-};
+    selects.forEach((item, index) => {
+      path.push(item)
+      dfs(path, selects.filter((item, index2) => {
+        return index2 !== index
+      }))
+      path.pop()
+    })
+  }
+  dfs([], selects)
+  return res
+}
 console.log(combine(4, 2))
 
 const listData = [
-    { id: 1001, parentId: 0, name: 'AA' },
-    { id: 1002, parentId: 1001, name: 'BB' },
-    { id: 1003, parentId: 1001, name: 'CC' },
-    { id: 1004, parentId: 1003, name: 'DD' },
-    { id: 1005, parentId: 1003, name: 'EE' },
-    { id: 1006, parentId: 1002, name: 'FF' },
-    { id: 1007, parentId: 1002, name: 'GG' },
-    { id: 1008, parentId: 1004, name: 'HH' },
-    { id: 1009, parentId: 1005, name: 'II' },
-    { id: 1010, parentId: 0, name: 'JJ' },
+  { id: 1001, parentId: 0, name: 'AA' },
+  { id: 1002, parentId: 1001, name: 'BB' },
+  { id: 1003, parentId: 1001, name: 'CC' },
+  { id: 1004, parentId: 1003, name: 'DD' },
+  { id: 1005, parentId: 1003, name: 'EE' },
+  { id: 1006, parentId: 1002, name: 'FF' },
+  { id: 1007, parentId: 1002, name: 'GG' },
+  { id: 1008, parentId: 1004, name: 'HH' },
+  { id: 1009, parentId: 1005, name: 'II' },
+  { id: 1010, parentId: 0, name: 'JJ' },
 ]
 
-function printTree(list) {
-    var tree = []
-    function getTree(list, tree) {
-        if (!list || list.length === 0) return
-        if (tree.length === 0) {
-            var roots = list.filter(item => item.parentId === 0).map(item => {
-                item.layerNo = 0
-                return item
-            })
-            tree.push(...roots)
-        }
-        tree.forEach(node => {
-            var qianzhui = new Array(node.layerNo).fill('&nbsp;')
-            console.log(`${qianzhui.join('')}${node.name}`)
-            var childrens = list.filter(item => item.parentId === node.id)
-            node.children = childrens.map(item => {
-                item.layerNo = node.layerNo + 1
-                return item
-            })
-            if (node.children.length > 0) getTree(list.filter(item => item.parentId !== node.id), node.children)
-        })
+function printTree (list) {
+  var tree = []
+  function getTree (list, tree) {
+    if (!list || list.length === 0) return
+    if (tree.length === 0) {
+      var roots = list.filter(item => item.parentId === 0).map(item => {
+        item.layerNo = 0
+        return item
+      })
+      tree.push(...roots)
     }
-    getTree(list, tree)
+    tree.forEach(node => {
+      var qianzhui = new Array(node.layerNo).fill('&nbsp;')
+      console.log(`${qianzhui.join('')}${node.name}`)
+      var childrens = list.filter(item => item.parentId === node.id)
+      node.children = childrens.map(item => {
+        item.layerNo = node.layerNo + 1
+        return item
+      })
+      if (node.children.length > 0) getTree(list.filter(item => item.parentId !== node.id), node.children)
+    })
+  }
+  getTree(list, tree)
 }
 printTree(listData)
